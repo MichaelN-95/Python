068f1610fb42c5ef20a965bcd79f4f381bdce369
Michael Norman <michaelnorman4421@gmail.com>
2017-01-31 21:31:46 +0000

    Added Comment

---
+++ PythonBasics/Printing,Inputs Etc/SpeakForMe.py
@@ -0,0 +1,9 @@
+
+print('Hello')
+name = "Michael"
+age = 21
+location = "Carlow"
+
+# Cannot add ints to print function with string also in it cast by using str(variable) function
+print('Your name is ' + name + ' Your age is ' + str(age) + ' You live in ' + location)
+print("Goodbye " + name)
